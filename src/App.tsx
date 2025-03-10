4ead82265ad37a39e5030b62d28121387e7a6255
tontona123 <148251226+tontona123@users.noreply.github.com>
2025-03-10 13:26:43 +0700

    Initial commit - Upload my project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Project</h1>
+      <p>This is the original version of App.tsx.</p>
+    </div>
+  );
+};
+
+export default App;
