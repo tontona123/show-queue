17fc3811fddef80096becdbf3808704a0b555b6d
tontona123 <148251226+tontona123@users.noreply.github.com>
2025-03-10 16:42:22 +0700

    [Update] ปรบ UI การใชบรการ Self-Practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./index.css";
 import EduRules from "./components/rules";
+import Edupractice from "./components/self-practice";
+import Eduwork from "./components/request-work";
+import Eduborrowing from "./components/borrowing";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <h1>Welcome to My Project</h1>
       <p>This is the original version of App.tsx.</p>
       <EduRules/>
+      <Edupractice/>
+      <Eduwork/>
+      <Eduborrowing/>
     </div>
   );
 };
